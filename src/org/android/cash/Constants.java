89b2203b10ad83856764fdec98642343ddd512cd
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-23 19:07:49 -0430

    Adding new constants to application. These ones will be useful to table names in database, including its columns. The interface now extends BaseColumns. This fact is useful when working with _ID constant in other classes.

--- src/org/android/cash/Constants.java
+++ src/org/android/cash/Constants.java
@@ -1,6 +1,11 @@
 package org.android.cash;
 
-public interface Constants {
+import android.provider.BaseColumns;
+
+public interface Constants extends BaseColumns {
 	public static int VIBRATION_ERROR = 80;
 	public static int VIBRATION_INTENT = 30;
+	public static String ACCOUNTS_TABLE = "accounts";
+	public static String TITLE = "title";
+	public static String TYPE = "type";
 }
