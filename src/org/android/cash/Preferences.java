16a02a18412120537e594d4235cd8dc2e8463cd4
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-23 12:50:48 -0430

    Adding a method to get the current value of vibration preference.

--- src/org/android/cash/Preferences.java
+++ src/org/android/cash/Preferences.java
@@ -1,14 +1,22 @@
 package org.android.cash;
 
-import org.android.cash.R;
-
+import android.content.Context;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
 
 public class Preferences extends PreferenceActivity {
+	private static String OPT_VIBRATION = "vibration";
+	private static boolean OPT_VIBRATION_DEF = true;
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+	}
+
+	public static boolean getVibration(Context context) {
+		return PreferenceManager.getDefaultSharedPreferences(context)
+				.getBoolean(OPT_VIBRATION, OPT_VIBRATION_DEF);
 	}
 }
