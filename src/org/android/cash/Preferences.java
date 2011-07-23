83de68d99769152948a2b41dda9923a9a0f1b71a
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-23 12:43:46 -0430

    Adding the preferences Java file to start preferences screen.

---
+++ src/org/android/cash/Preferences.java
@@ -0,0 +1,14 @@
+package org.android.cash;
+
+import org.android.cash.R;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Preferences extends PreferenceActivity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+	}
+}
