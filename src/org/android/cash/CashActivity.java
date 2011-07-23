5da4e81871d54b7ceeae10361d7ac99121e22af4
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-23 12:35:21 -0430

    Renaming again the packages in order to make it better.

---
+++ src/org/android/cash/CashActivity.java
@@ -0,0 +1,39 @@
+package org.android.cash;
+
+import org.android.cash.R;
+import org.android.cash.info.Preferences;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+public class CashActivity extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.menu, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.preferences:
+			Intent intent = new Intent(this, Preferences.class);
+			startActivity(intent);
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
+}
