1a5cf22388b0198b72c2dc21bae76a5a25dc371b
Richard Ricciardelli <ricciardelli2021@gmail.com>
2011-07-23 19:09:32 -0430

    Adding the first version of the Accounts module. It does not have its own graphic user interface yet because it is under development.

---
+++ src/org/android/app/Accounts.java
@@ -0,0 +1,12 @@
+package org.android.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Accounts extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// setContentView(R.layout.accounts);
+	}
+}
